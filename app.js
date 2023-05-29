6911d8670f73f0b3dd6b3080fc65afadaad97c60
mguney134 <mguney@protonmail.com>
2023-05-30 00:25:17 +0200

    new message add

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const https = require("https");
 const PORT = 3000;
 
 const server = https.createServer(function (req, res) {
-    res.write("Hello, My name Mustafa Guney. I am Full Stack Developer!");
+    res.write("Hello, My name Mustafa Guney. I am Full Stack Developer! This is my blog page.");
     res.end();
 });
 
